c93a54353b434e77c78168548a1c4aaa184e57db
iluque95 <iludi22@hotmail.es>
2020-02-26 18:28:26 +0100

    Fixed some pointers and added function to separate name from guild.

--- minimapa/pch.cpp
+++ minimapa/pch.cpp
@@ -6,11 +6,13 @@
 #include <thread>
 #include <string>
 #include <condition_variable>
+#include <sstream>
 
 // Cuando se utilizan encabezados precompilados, se requiere este archivo de código fuente para que la compilación se realice correctamente.
 
 /*
 Public Type tMapBlock
+
 	Blocked    As Byte
 
 	Graphic(1 To 4) As Grh
@@ -22,8 +24,39 @@ Public Type tMapBlock
 	TileExit   As WorldPos
 
 	Trigger    As Integer
+
 End Type
 
+*/
+
+/*
+
+	Public Type Grh
+
+		Frames     As Integer		2
+		Frame      As Integer		4
+		LastRun    As Long			8
+		GrhIndex   As Integer		10
+		DelayMS    As Integer		12
+
+	End Type
+
+*/
+
+/*
+
+	Party as byte			0
+	PartyNum as byte		2
+	Position as Position	4
+	Nombre as string		8
+
+*/
+
+/*
+	Public Type Position
+		x          As Integer
+		y          As Integer
+	End Type
 */
 
 
@@ -31,7 +64,7 @@ End Type
 HDC phdc;
 bool run = false;
 
-bool* VTecho = false;
+int32_t VTechoPTR;
 
 int32_t MapDataPTR;
 uint16_t MapDataSize;
@@ -53,7 +86,7 @@ std::mutex m_;
 bool data_is_ready_{};
 
 
-void CALLBACK CRAW_API_Init(int32_t hwnd, int32_t MapData, int16_t MDLen, int32_t CharList, int16_t CLLen, int32_t ColorMap, int16_t UserIndex)
+void CALLBACK CRAW_API_Init(int32_t hwnd, int32_t techo, int32_t MapData, int16_t MDLen, int32_t CharList, int16_t CLLen, int32_t ColorMap, int16_t UserIndex)
 {
 	phdc = GetDC((HWND)hwnd);
 
@@ -70,6 +103,8 @@ void CALLBACK CRAW_API_Init(int32_t hwnd, int32_t MapData, int16_t MDLen, int32_
 	uint32_t offset = (UserIndex - 1) * CLLen;
 
 	UserCharIndex = CharListPTR + offset;
+
+	VTechoPTR = techo;
 }
 
 void Draw_Point(uint8_t x, uint8_t y, COLORREF color)
@@ -79,6 +114,19 @@ void Draw_Point(uint8_t x, uint8_t y, COLORREF color)
 	SetPixel(phdc, x - 1, y, color);
 	SetPixel(phdc, x, y - 1, color);
 	SetPixel(phdc, x, y + 1, color);
+}
+
+
+std::string getGuild(std::string& name)
+{
+
+	return name.substr(name.find('<'), name.length());
+}
+
+std::string getName(std::string& name)
+{
+
+	return name.substr(0, name.find('<'));
 }
 
 void Render_Minimap()
@@ -98,7 +146,7 @@ void Render_Minimap()
 
 					auto pos = MapDataPTR + (i)* MapDataSize + (j) * 100;
 
-					++pos; // Overtake "blocked" from struct.
+					pos += sizeof(int16_t) + 10; // Overpass "blocked" from struct and move to GrhIndex
 
 					auto ptr = reinterpret_cast<int16_t*>(pos);
 
@@ -115,82 +163,74 @@ void Render_Minimap()
 						SetPixel(phdc, i + 1, j + 1, *(color));
 					}
 
-					if (VTecho)
+					auto VTecho = reinterpret_cast<uint8_t*>(VTechoPTR);
+
+					if (*VTecho)
 					{
-						int16_t grhindex2 = *(ptr + 2);
 
-						if (grhindex2)
+						for (auto k = 2U; k <= 4; ++k)
 						{
-							auto offset = (grhindex2 - 1) * sizeof(int32_t);
+							int16_t grhindex = *(ptr+(sizeof(int16_t)*k)+10);
 
-							auto color_pos = ColorMapPTR + offset;
+							if (grhindex)
+							{
+								auto offset = (grhindex - 1) * sizeof(int32_t);
 
-							auto color = reinterpret_cast<int32_t*>(color_pos);
+								auto color_pos = ColorMapPTR + offset;
 
-							SetPixel(phdc, i + 1, j + 1, *(color));
-						}
+								auto color = reinterpret_cast<int32_t*>(color_pos);
 
-						int16_t grhindex3 = *(ptr + 4);
-
-						if (grhindex3)
-						{
-							auto offset = (grhindex3 - 1) * sizeof(int32_t);
-
-							auto color_pos = ColorMapPTR + offset;
-
-							auto color = reinterpret_cast<int32_t*>(color_pos);
-
-							SetPixel(phdc, i + 1, j + 1, *(color));
-						}
-
-						int16_t grhindex4 = *(ptr + 6);
-
-						if (grhindex4)
-						{
-							auto offset = (grhindex4 - 1) * sizeof(int32_t);
-
-							auto color_pos = ColorMapPTR + offset;
-
-							auto color = reinterpret_cast<int32_t*>(color_pos);
-
-							SetPixel(phdc, i + 1, j + 1, *(color));
+								SetPixel(phdc, i + 1, j + 1, *(color));
+							}
 						}
 					}
 				}
 			}
 
-			// DRAW USER POINT
+			// DRAW USERS POINT
 
 			for (auto i = 0U; i < 10000; ++i)
 			{
-				auto pos = CharListPTR + i;
+				auto pos = CharListPTR + (i* CharListSize);
 
 				auto ptr = reinterpret_cast<int16_t*>(pos);
 
 				if (pos != UserCharIndex)
 				{
-					auto guild = pos;
-					auto party = pos;
+					auto party = reinterpret_cast<int16_t*>(pos);
+					auto party_num = reinterpret_cast<int16_t*>(pos + 2); // sizeof(int16_t)
+					auto x = reinterpret_cast<int16_t*>(pos + 4); 
+					auto y = reinterpret_cast<int16_t*>(pos + 6);
+					auto guild = reinterpret_cast<char*>(pos + 8);
 
-					auto user_guild = UserCharIndex + 10; // FIX OFFSETS
-					auto user_party = UserCharIndex + 15;
+					std::stringstream ssstream;
+					ssstream << guild;
+					std::string str = ssstream.str();
+					
 
+					auto user_party = reinterpret_cast<int16_t*>(UserCharIndex);
+					auto user_party_num = reinterpret_cast<int16_t*>(UserCharIndex + 2);
+					auto user_guild = reinterpret_cast<char *>(UserCharIndex + 8);
 
-					if (guild = user_guild)
+					std::stringstream user_stream;
+					user_stream << user_guild;
+					std::string str_user = user_stream.str();
+
+					if (getGuild(str) == getGuild(str_user))
 					{
-
-						auto x = 100;
-						auto y = 200;
-
-						Draw_Point(x, y, RGB(255, 236, 51));
+						Draw_Point(*x, *y, RGB(255, 236, 51));
 					}
-					else if (party = user_party)
+					else if (*party > 0 && *party_num == *user_party_num)
 					{
-						auto x = 100;
-						auto y = 200;
-
-						Draw_Point(x, y, RGB(221, 51, 255));
+						Draw_Point(*x, *y, RGB(221, 51, 255));
 					}
+				}
+				else
+				{
+					auto user_x = reinterpret_cast<int16_t*>(UserCharIndex + 4);
+					auto user_y = reinterpret_cast<int16_t*>(UserCharIndex + 6);
+
+					Draw_Point(*user_x, *user_y, RGB(255, 0, 0));
 				}
 			}
 
